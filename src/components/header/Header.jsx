cc743277f6b95c5a1063f6cf4ae369f2f0dce44c
Danilo Sampaio <daniloarpesam@gmail.com>
2020-05-11 12:42:46 -0300

    added header paragraph and link to job positions

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,10 +4,21 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>
+    <div className="header-container container">
       <div className="header">
         <h1>Trabalhe no Elo7</h1>
       </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
+          Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
+          metus leo nec est. Curabitur placerat lorem vel volutpat efficitur. 
+        </p>
+      </div>
+      <hr></hr>
+      <a href="https://elo7.gupy.io/" className="link-open-positions" >
+        Vagas em Aberto &#8811;
+      </a>
     </div>
   )
 }
