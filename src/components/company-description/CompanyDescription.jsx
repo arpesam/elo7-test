a651cf74cc003bb3eea225887489e0c608924f74
Danilo Sampaio <daniloarpesam@gmail.com>
2020-05-11 14:49:58 -0300

    added bottom image

--- src/components/company-description/CompanyDescription.jsx
+++ src/components/company-description/CompanyDescription.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Card from './card/Card.jsx'
 import { cardData } from './company-description'; 
-
+import companyParty from '../../assets/images/foto-bottom.png'
 import './CompanyDescription.css'
 
 
 const CompanyDescription = () => {
-
-
   return (
     <div className="company-description">
       <div className="cards-wrapper flex-row">
@@ -24,6 +22,7 @@ const CompanyDescription = () => {
       >
         Saiba Mais &#8811;
       </a>
+      <img src={companyParty} alt="company-party" style={{ margin: '40px 0' }}/>
     </div>
   )
 }
