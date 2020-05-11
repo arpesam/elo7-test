0320192e6172395742f73c5b4b157a07b9bad3d5
Danilo Sampaio <daniloarpesam@gmail.com>
2020-05-11 18:53:26 -0300

    Updates

---
+++ precache-manifest.34b1b8e4ac66fde3b699c7e02d8e0017.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde7c6e900e74cfa24f6a58826496fb9",
+    "url": "/elo7-test/index.html"
+  },
+  {
+    "revision": "01c2dbdfa02f0b6ddd7c",
+    "url": "/elo7-test/static/css/main.84b3ded2.chunk.css"
+  },
+  {
+    "revision": "aca0b5a18fe2b801a97b",
+    "url": "/elo7-test/static/js/2.5ff93611.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elo7-test/static/js/2.5ff93611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c2dbdfa02f0b6ddd7c",
+    "url": "/elo7-test/static/js/main.e72924ff.chunk.js"
+  },
+  {
+    "revision": "43c0e9827e4e26df1176",
+    "url": "/elo7-test/static/js/runtime-main.31edaf3d.js"
+  },
+  {
+    "revision": "f4858a6e9de2f3e1211721b77b1be59c",
+    "url": "/elo7-test/static/media/beatriz.f4858a6e.png"
+  },
+  {
+    "revision": "b3e5942004c3a2b8d759b294cf46161b",
+    "url": "/elo7-test/static/media/camila.b3e59420.png"
+  },
+  {
+    "revision": "2fbeb7a9ad7deea0f44e7a269d2b4153",
+    "url": "/elo7-test/static/media/david.2fbeb7a9.png"
+  },
+  {
+    "revision": "401fce37544ebc1d484fcdb6d111061c",
+    "url": "/elo7-test/static/media/foto-bottom.401fce37.png"
+  },
+  {
+    "revision": "5a172d1fac5ee4335e57eaf8f692ff4a",
+    "url": "/elo7-test/static/media/foto-header.5a172d1f.png"
+  },
+  {
+    "revision": "2117ab78cb80b7dc9661d3cf8beb2d52",
+    "url": "/elo7-test/static/media/guto.2117ab78.png"
+  }
+]);
