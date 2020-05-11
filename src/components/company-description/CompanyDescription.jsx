325a2a13f68aa1e0e139d3e39a049b7d934fa8c0
Danilo Sampaio <daniloarpesam@gmail.com>
2020-05-11 14:44:16 -0300

    added more info link and horizontal line at company description bottom

--- src/components/company-description/CompanyDescription.jsx
+++ src/components/company-description/CompanyDescription.jsx
@@ -1,42 +1,29 @@
 import React from 'react'
 import Card from './card/Card.jsx'
-import lifeQuality from '../../assets/images/qualidade.png'
-import relaxed from '../../assets/images/descontracao.png'
-import activity from '../../assets/images/atividades.png'
+import { cardData } from './company-description'; 
+
 import './CompanyDescription.css'
 
 
 const CompanyDescription = () => {
-  const cardData = [
-    {
-      title: 'Qualidade de vida',
-      img: lifeQuality,
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
-      Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
-      metus.`
-    },
-    {
-      title: 'Embiente descontraído',
-      img: relaxed,
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
-      Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
-      metus leo nec est. Curabitur placerat lorem vel volutpat efficitur. Lorem ipsum dolor sit 
-      amet.`
-    },
-    {
-      title: 'Atividades extras',
-      img: activity,
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
-      Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
-      metus leo nec est. Curabitur placerat lorem vel.`
-    }
-  ]
+
 
   return (
     <div className="company-description">
-      {cardData.map((card, index) => (
-        <Card key={index} {...card} />
-      ))}
+      <div className="cards-wrapper flex-row">
+        {cardData.map((card, index) => (
+          <Card key={index} {...card} />
+        ))}
+      </div>
+      <hr />
+      <a
+        href="https://www.linkedin.com/company/elo7/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="to-uppercase link-more-info"
+      >
+        Saiba Mais &#8811;
+      </a>
     </div>
   )
 }
---
+++ src/components/company-description/company-description.js
@@ -0,0 +1,28 @@
+import lifeQuality from '../../assets/images/qualidade.png'
+import relaxed from '../../assets/images/descontracao.png'
+import activity from '../../assets/images/atividades.png'
+
+export const cardData = [
+  {
+    title: 'Qualidade de vida',
+    img: lifeQuality,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
+    Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
+    metus.`
+  },
+  {
+    title: 'Embiente descontraído',
+    img: relaxed,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
+    Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
+    metus leo nec est. Curabitur placerat lorem vel volutpat efficitur. Lorem ipsum dolor sit 
+    amet.`
+  },
+  {
+    title: 'Atividades extras',
+    img: activity,
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur porta mauris turpis.
+    Vestibulum bibendum, tortor tempus dapibus egestas, diam elit sagittis urna, non efficitur
+    metus leo nec est. Curabitur placerat lorem vel.`
+  }
+]
